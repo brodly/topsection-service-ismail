af55585e5ebd1b20b376b6f780216e0ed35827d2
armouti <alarmouti.ismail@gmail.com>
2018-11-09 17:38:42 -0600

    extracted TopSection Component out of CoursePage so it fits better with other components.

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 // import 'semantic-ui-css/semantic.min.css';
-
-ReactDOM.render(<App />, document.getElementById('App'));
+window.TopSection = App;
+// ReactDOM.render(<App />, document.getElementById('App'));
