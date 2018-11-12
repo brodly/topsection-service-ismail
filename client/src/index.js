bd8d77ecdc62be0a01a4d2fa9c2b40816b95e4ab
armouti <alarmouti.ismail@gmail.com>
2018-11-12 11:41:37 -0600

    q

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 // import 'semantic-ui-css/semantic.min.css';
-window.TopSection = App;
-// ReactDOM.render(<App />, document.getElementById('App'));
+window.Topsection = App;
