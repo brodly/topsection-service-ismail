945e5116b778b6c711bb0fd16076c952ceebf1eb
Joshua Marx <marx.joshua@gmail.com>
2018-11-14 15:31:00 -0800

    refactoring database to work with postgres

--- db/db.js
+++ db/db.js
@@ -1,57 +1,50 @@
-const mysql = require('mysql');
+const { client } = require('./index');
 
-const db_user = process.env.MYSQL_USER || 'root';
-const db_pass = process.env.MYSQL_PASSWORD || 'password';
-
-const connection = mysql.createConnection({
-  host: 'topsectiondb',
-  user: db_user,
-  password: db_pass,
-})
-
-connection.connect((err) => {
-  if (err) console.log(err)
+client((err) => {
+  if (err) console.log(err);
 });
 
-connection.queryAsync = function(query, options){
-  const thisPool = this;
-  return new Promise((resolve, reject) => {
-    thisPool.query(query, options, (err, results, fields) => {
-      if(err) reject(err);
-      else resolve(results, fields)
-    });
-  }); 
-}
-connection.queryAsync('CREATE DATABASE IF NOT EXISTS udemy;')
-.then(() => connection.queryAsync('USE udemy;'))
-.then(() => (
-  connection.queryAsync(`
-  CREATE TABLE IF NOT EXISTS courses (
-      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-      title VARCHAR(255),
-      subtitle VARCHAR(255),
-      teacher_names VARCHAR(255),
-      avg_rating INT,
-      rating_count INT,
-      student_count INT,
-      last_updated DATE,
-      thumbnail_img TEXT,
-      price INT,
-      lang VARCHAR(255),
-      subtitle_lang VARCHAR(255),
-      course_len FLOAT,
-      isOnDiscount BOOLEAN,
-      current_price FLOAT,
-      discount FLOAT,
-      num_of_articles INT,
-      dwl_resources_count INT,
-      discountCountdown VARCHAR(12),
-      hasTag BOOLEAN,
-      tag VARCHAR(30)
-    );
-  `)
-))
-.catch((error) => console.log('error configuring db', error));
+// connection.queryAsync = function queryAsync(query, options) {
+//   const thisPool = this;
+//   return new Promise((resolve, reject) => {
+//     thisPool.query(query, options, (err, results, fields) => {
+//       if (err) reject(err);
+//       else resolve(results, fields);
+//     });
+//   });
+// };
+
+// connection.queryAsync('CREATE DATABASE IF NOT EXISTS udemy;')
+//   .then(() => connection.queryAsync('USE udemy;'))
+//   .then(() => (
+//     connection.queryAsync(`
+//     CREATE TABLE IF NOT EXISTS courses (
+//         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//         title VARCHAR(255),
+//         subtitle VARCHAR(255),
+//         teacher_names VARCHAR(255),
+//         avg_rating INT,
+//         rating_count INT,
+//         student_count INT,
+//         last_updated DATE,
+//         thumbnail_img TEXT,
+//         price INT,
+//         lang VARCHAR(255),
+//         subtitle_lang VARCHAR(255),
+//         course_len FLOAT,
+//         isOnDiscount BOOLEAN,
+//         current_price FLOAT,
+//         discount FLOAT,
+//         num_of_articles INT,
+//         dwl_resources_count INT,
+//         discountCountdown VARCHAR(12),
+//         hasTag BOOLEAN,
+//         tag VARCHAR(30)
+//       );
+//     `)
+//   ))
+//   .catch(error => console.log('error configuring db', error));
+
 // let queryAsync = function (query, options = null) {
 //   let connect = this;
 //   return new Promise((resolve, reject) => {
@@ -63,11 +56,11 @@ connection.queryAsync('CREATE DATABASE IF NOT EXISTS udemy;')
 // };
 
 // connection.on('connection', (error, connection) => {
-  // if (error) console.log('error on.connection', error);
-  // connection.getConnection((err, con) => {
-  //   if (error) console.log('error getConneciton', err);
-  //   else {
-    // }
+// if (error) console.log('error on.connection', error);
+// connection.getConnection((err, con) => {
+//   if (error) console.log('error getConneciton', err);
+//   else {
+// }
 // });
 
 module.exports = { connection };
--- db/seed.js
+++ db/seed.js
@@ -1,172 +1,128 @@
-const { Pool, Client } = require('pg');
 const faker = require('faker');
-// const mysql = require('mysql');
+const db = require('./index');
 
-const config = {
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  port: 32,
-  database: 'udemy',
-};
+db.client();
 
-const pool = new Pool({
-  user: config.user,
-  host: config.host,
-  database: config.database,
-  password: config.password,
-  port: config.port,
-});
+// const queryAsync = (query, options = null) => {
+//   return new Promise((resolve, reject) => {
+//     db.query(query, options, (err, results, fields) => {
+//       if (err) reject(err);
+//       else resolve(results, fields);
+//     });
+//   });
+// };
 
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
-  pool.end();
-});
+// queryAsync(`
+//   CREATE TABLE IF NOT EXISTS courses (
+//     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//     title VARCHAR(255),
+//     subtitle VARCHAR(255),
+//     teacher_names VARCHAR(255),
+//     avg_rating INT,
+//     rating_count INT,
+//     student_count INT,
+//     last_updated DATE,
+//     thumbnail_img TEXT,
+//     price INT,
+//     lang VARCHAR(255),
+//     subtitle_lang VARCHAR(255),
+//     course_len FLOAT,
+//     isOnDiscount BOOLEAN,
+//     current_price FLOAT,
+//     discount FLOAT,
+//     num_of_articles INT,
+//     dwl_resources_count INT,
+//     discountCountdown VARCHAR(12),
+//     hasTag BOOLEAN,
+//     tag VARCHAR(30)
+//   );
+// `)
+//   .then(() => {
+//     const parseData = (input) => {
+//       return Object.entries(input).reduce((parsed, [key, val], index, entries) => {
+//         val = typeof val === 'string' ? `"${val}"` : val;
+//         if (index === (entries.length - 1)) {
+//           parsed.fields += `${key}`;
+//           parsed.placeholders += `?`;
+//         }
+//         else {
+//           parsed.fields += `${key}, `;
+//           parsed.placeholders += `?, `;
+//         }
+//         parsed.values.push(val);
+//         return parsed;
+//       }, { fields: '', values: [] , placeholders: '' })
+//     };
 
-const client = new Client({
-  user: config.user,
-  host: config.host,
-  database: config.database,
-  password: config.password,
-  port: config.port,
-});
+//     for (let i = 0; i < 200; i += 1) {
+//       const studentCount = faker.random.number();
+//       const ratingCount = studentCount * 0.4;
+//       const randomBinary = () => Math.floor(Math.random() * 2);
+//       const fullName = () => `${faker.name.firstName()} ${faker.name.lastName()}`;
+//       const rand = Math.random;
+//       const floor = Math.floor;
 
-client.connect();
+//       const seedObj = {
+//         title: `${faker.hacker.verb()} ${faker.name.jobTitle()}`,
+//         subtitle: 'This course will teach you ALL you need about this job, so you can be armed with all the knowledge you need!',
+//         teacher_names: `${fullName()}_${fullName()}`,
+//         avg_rating: floor(rand() * 5),
+//         rating_count: ratingCount,
+//         student_count: studentCount,
+//         last_updated: randomBinary() ? faker.date.recent() : faker.date.past(),
+//         thumbnail_img: faker.image.imageUrl(),
+//         price: floor(faker.commerce.price()),
+//         lang: randomBinary() ? randomBinary() ? 'English' : 'Spanish' : 'Arabic',
+//         subtitle_lang: randomBinary() ? randomBinary() ? 'Spanish' : 'Arabic' : 'English',
+//         course_len: Number((rand() * 50).toFixed(2)),
+//         isOnDiscount: Boolean(randomBinary()),
+//         current_price: Number((10 + rand() * 20).toFixed(2)),
+//         discount: Number(rand().toFixed(2)),
+//         num_of_articles: floor(rand() * 200),
+//         dwl_resources_count: floor(rand() * 200),
+//         discountCountdown: `${floor(rand() * 10)} days`,
+//         hasTag: Boolean(randomBinary()),
+//         tag:  randomBinary() ? randomBinary() ? 'BESTSELLER' : 'HOT' : 'NEW!',
+//       };
 
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
-  client.end();
-})
+//       const parsedData = parseData(seedObj);
+//       queryAsync(`INSERT INTO courses (${parsedData.fields}) VALUES (${parsedData.placeholders})`, parsedData.values)
+//         .catch((e) => { console.log('error saving course instance: ', e); });
+//     }
+//   });
 
+// const Course = db.define('course', {
+//   id: {
+//     type: Sequelize.STRING, primaryKey: true, autoInrement: true
+//   },
+//   title: Sequelize.STRING,
+//   subtitle: Sequelize.STRING,
+//   teacher_name: Sequelize.STRING,
+//   avg_rating: Sequelize.INT,
+//   rating_count: Sequelize.INT,
+//   student_count: Sequelize.INT,
+//   last_updated: Sequelize.DATE,
+//   thumbnail_img: Sequelize.TEXT,
+//   lang: Sequelize.TEXT,
+//   price: Sequelize.INT,
+//   subtitle_lang: Sequelize.STRING,
+//   course_len: Sequelize.INT,
+// })
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'udemy',
-});
-
-db.connect();
-
-const queryAsync = function (query, options = null) {
-  return new Promise((resolve, reject) => {
-    db.query(query, options, (err, results, fields) => {
-      if(err) reject(err);
-      else resolve(results, fields);
-    });
-  });
-};
-
-queryAsync(`
-  CREATE TABLE IF NOT EXISTS courses (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
-    subtitle VARCHAR(255),
-    teacher_names VARCHAR(255),
-    avg_rating INT,
-    rating_count INT,
-    student_count INT,
-    last_updated DATE,
-    thumbnail_img TEXT,
-    price INT,
-    lang VARCHAR(255),
-    subtitle_lang VARCHAR(255),
-    course_len FLOAT,
-    isOnDiscount BOOLEAN,
-    current_price FLOAT,
-    discount FLOAT,
-    num_of_articles INT,
-    dwl_resources_count INT,
-    discountCountdown VARCHAR(12),
-    hasTag BOOLEAN,
-    tag VARCHAR(30)
-  );
-`)
-  .then(() => {
-    const parseData = input => {
-      return Object.entries(input).reduce((parsed, [ key, val ], index, entries) => {
-        val = typeof val === 'string' ? `"${val}"` : val;
-        if (index === (entries.length - 1)) {
-          parsed.fields += `${key}`;
-          parsed.placeholders += `?`;
-        }
-        else {
-          parsed.fields += `${key}, `;
-          parsed.placeholders += `?, `;
-        }
-        parsed.values.push(val);
-        return parsed;
-      }, { fields: '', values: [] , placeholders: '' })
-    };
-
-    for (let i = 0; i < 200; i += 1) {
-      const studentCount = faker.random.number();
-      const ratingCount = studentCount * 0.4;
-      const randomBinary = () => Math.floor(Math.random() * 2);
-      const fullName = () => faker.name.firstName() + ' ' + faker.name.lastName();
-      const rand = Math.random;
-      const floor = Math.floor;
-
-      const seedObj = {
-        title: faker.hacker.verb() + ' ' + faker.name.jobTitle(),
-        subtitle: `This course will teach you ALL you need about this job, so you can be armed with all the knowledge you need.!`,
-        teacher_names: `${fullName()}_${fullName()}`,
-        avg_rating: floor(rand() * 5),
-        rating_count: ratingCount,
-        student_count: studentCount,
-        last_updated: randomBinary() ? faker.date.recent() : faker.date.past(),
-        thumbnail_img: faker.image.imageUrl(),
-        price: floor(faker.commerce.price()),
-        lang: randomBinary() ? randomBinary() ? 'English' : 'Spanish' : 'Arabic',
-        subtitle_lang: randomBinary() ? randomBinary() ? 'Spanish' : 'Arabic' : 'English',
-        course_len: Number((rand() * 50).toFixed(2)),
-        isOnDiscount: Boolean(randomBinary()),
-        current_price: Number((10 + rand() * 20).toFixed(2)),
-        discount: Number(rand().toFixed(2)),
-        num_of_articles: floor(rand() * 200),
-        dwl_resources_count: floor(rand() * 200),
-        discountCountdown: `${floor(rand() * 10)} days`,
-        hasTag: Boolean(randomBinary()),
-        tag:  randomBinary() ? randomBinary() ? 'BESTSELLER' : 'HOT' : 'NEW!',
-      };
-
-      const parsedData = parseData(seedObj);
-      queryAsync(`INSERT INTO courses (${parsedData.fields}) VALUES (${parsedData.placeholders})`, parsedData.values)
-        .catch((e) => console.log('error saving course instance: ', e));
-    }
-  });
-
-  // const Course = db.define('course', {
-  //   id: {
-  //     type: Sequelize.STRING, primaryKey: true, autoInrement: true
-  //   },
-  //   title: Sequelize.STRING,
-  //   subtitle: Sequelize.STRING,
-  //   teacher_name: Sequelize.STRING,
-  //   avg_rating: Sequelize.INT,
-  //   rating_count: Sequelize.INT,
-  //   student_count: Sequelize.INT,
-  //   last_updated: Sequelize.DATE,
-  //   thumbnail_img: Sequelize.TEXT,
-  //   lang: Sequelize.TEXT,
-  //   price: Sequelize.INT,
-  //   subtitle_lang: Sequelize.STRING,
-  //   course_len: Sequelize.INT,
-  // })
-    // Course.create({
-    //   title: faker.hacker.verb() + ' ' + faker.name.jobTitle(),
-    //   subtitle: `This course will teach you ALL you need about this!`,
-    //   teacher_name: faker.name.firstName() + ' ' + faker.name.lastName(),
-    //   avg_rating: Math.random() * 5,
-    //   rating_count: studentCount,
-    //   student_count: ratingCount,
-    //   last_update:  Math.floor(Math.random() * 2) ? faker.date.recent() : faker.date.past(),
-    //   thumbnail_img: faker.image.imageUrl(),
-    //   lang: Math.floor(Math.random() * 2) ? Math.floor(Math.random() * 2) ? 'English' : 'Spanish' : 'Arabic',
-    //   price: faker.commerce.price(),
-    //   subtitiles_lang: Math.floor(Math.random() * 2) ? Math.floor(Math.random() * 2) ? 'Spanish' : 'Arabic' : 'English',
-    //   course_len: `${Math.floor(Math.random() *)} Hours`
-    // })
+// Course.create({
+//   title: faker.hacker.verb() + ' ' + faker.name.jobTitle(),
+//   subtitle: `This course will teach you ALL you need about this!`,
+//   teacher_name: faker.name.firstName() + ' ' + faker.name.lastName(),
+//   avg_rating: Math.random() * 5,
+//   rating_count: studentCount,
+//   student_count: ratingCount,
+//   last_update:  Math.floor(Math.random() * 2) ? faker.date.recent() : faker.date.past(),
+//   thumbnail_img: faker.image.imageUrl(),
+//   lang: Math.floor(Math.random() * 2) ? Math.floor(Math.random() * 2) ? 'English' : 'Spanish' : 'Arabic',
+//   price: faker.commerce.price(),
+//   subtitiles_lang: Math.floor(Math.random() * 2) ? Math.floor(Math.random() * 2) ? 'Spanish' : 'Arabic' : 'English',
+//   course_len: `${Math.floor(Math.random() *)} Hours`
+// })
 // const knex = require('knex')({
 //   client: 'pg',
 //   connection: {
