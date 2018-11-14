84ab8acf6418d21d5c7dc0e93aa62079369eb553
Joshua Marx <marx.joshua@gmail.com>
2018-11-14 12:05:36 -0800

    refactored to work with random entry from database

--- client/src/index.js
+++ client/src/index.js
@@ -3,3 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 // import 'semantic-ui-css/semantic.min.css';
 window.Topsection = App;
+
+ReactDOM.render(<App />, document.getElementById('App'));
