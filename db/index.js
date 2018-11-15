d5e6654a72d1957a76cb6e9ec8bdb459cffce5be
Joshua Marx <marx.joshua@gmail.com>
2018-11-14 21:15:01 -0800

    refactored to make a connection to postgres

--- db/index.js
+++ db/index.js
@@ -1,37 +1,12 @@
-const knex = require('knex');
-const { Pool, Client } = require('pg');
-const config = require('./config');
+const Knex = require('knex');
+const { pg: config } = require('./config');
 
-const db = knex();
-
-const connection = {
+module.exports.pg = Knex({
+  client: 'pg',
   user: config.user,
   host: config.host,
   database: config.database,
   password: config.password,
   port: config.port,
-};
-
-db({
-  client: 'pg',
-  version: '7.2',
-  connection,
+  pool: { min: 0, max: 7 },
 });
-
-const pool = new Pool(connection);
-const client = new Client(connection);
-
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
-  pool.end();
-});
-
-client.connect();
-
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
-  client.end();
-});
-
-module.exports.pool = pool;
-module.exports.client = client;
