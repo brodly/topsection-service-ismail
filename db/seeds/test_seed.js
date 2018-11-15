43345b968a788557ba1ef9d7ab379d4376cdbf24
Joshua Marx <marx.joshua@gmail.com>
2018-11-15 15:14:09 -0800

    implemented testing code to insert entries into db via csv

--- db/seeds/test_seed.js
+++ db/seeds/test_seed.js
@@ -1,37 +1,15 @@
-const faker = require('faker');
 const path = require('path');
+const { performance } = require('perf_hooks');
 
-const file = path.join(__dirname, '../generation/outputfile.csv');
-console.log(file);
+const file = 'outputfile.csv';
+const dir = path.join(__dirname, `../generation/${file}`);
+let t0;
+let t1;
 
 exports.seed = (knex, Promise) => {
-  // Deletes ALL existing entries
   return knex('courses').del()
-    .then(() => {
-      // Inserts seed entries
-      return knex.schema.raw(`COPY courses FROM ${file} WITH (FORMAT csv);`);
-
-      // return knex('courses').insert({
-      //   title: `${faker.hacker.verb()} ${faker.name.jobTitle()}`,
-      //   subtitle: 'This course will teach you ALL you need about this job, so you can be armed with all the knowledge you need!',
-      //   teacher_names: 'Ismail Alarmouti',
-      //   avg_rating: 4,
-      //   rating_count: 10,
-      //   student_count: 10,
-      //   last_updated: faker.date.recent(),
-      //   thumbnail_img: faker.image.imageUrl(),
-      //   price: Math.floor(faker.commerce.price()),
-      //   lang: 'English',
-      //   subtitle_lang: 'Spanish',
-      //   course_len: 20,
-      //   isOnDiscount: true,
-      //   current_price: 200,
-      //   discount: 10,
-      //   num_of_articles: 82,
-      //   dwl_resources_count: 35,
-      //   discountCountdown: '4 days',
-      //   hasTag: true,
-      //   tag: 'HOT',
-      // });
-    });
+    .then(() => { t0 = performance.now(); })
+    .then(() => knex.schema.raw(`COPY courses FROM '${dir}' WITH (FORMAT csv);`))
+    .then(() => { t1 = performance.now(); })
+    .then(() => { console.log(`Done! Seeding ${file} took ${t1 - t0} milliseconds`); });
 };
