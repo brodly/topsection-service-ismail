eb96d08cfb9b1a3aa635f2e6806a0de86c5e92cf
Joshua Marx <marx.joshua@gmail.com>
2018-11-15 12:39:03 -0800

    testing inserting generated data into db

--- db/seeds/test_seed.js
+++ db/seeds/test_seed.js
@@ -1,31 +1,37 @@
 const faker = require('faker');
+const path = require('path');
+
+const file = path.join(__dirname, '../generation/outputfile.csv');
+console.log(file);
 
 exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
   return knex('courses').del()
     .then(() => {
       // Inserts seed entries
-      return knex('courses').insert({
-        title: `${faker.hacker.verb()} ${faker.name.jobTitle()}`,
-        subtitle: 'This course will teach you ALL you need about this job, so you can be armed with all the knowledge you need!',
-        teacher_names: 'Ismail Alarmouti',
-        avg_rating: 4,
-        rating_count: 10,
-        student_count: 10,
-        last_updated: faker.date.recent(),
-        thumbnail_img: faker.image.imageUrl(),
-        price: Math.floor(faker.commerce.price()),
-        lang: 'English',
-        subtitle_lang: 'Spanish',
-        course_len: 20,
-        isOnDiscount: true,
-        current_price: 200,
-        discount: 10,
-        num_of_articles: 82,
-        dwl_resources_count: 35,
-        discountCountdown: '4 days',
-        hasTag: true,
-        tag: 'HOT',
-      });
+      return knex.schema.raw(`COPY courses FROM ${file} WITH (FORMAT csv);`);
+
+      // return knex('courses').insert({
+      //   title: `${faker.hacker.verb()} ${faker.name.jobTitle()}`,
+      //   subtitle: 'This course will teach you ALL you need about this job, so you can be armed with all the knowledge you need!',
+      //   teacher_names: 'Ismail Alarmouti',
+      //   avg_rating: 4,
+      //   rating_count: 10,
+      //   student_count: 10,
+      //   last_updated: faker.date.recent(),
+      //   thumbnail_img: faker.image.imageUrl(),
+      //   price: Math.floor(faker.commerce.price()),
+      //   lang: 'English',
+      //   subtitle_lang: 'Spanish',
+      //   course_len: 20,
+      //   isOnDiscount: true,
+      //   current_price: 200,
+      //   discount: 10,
+      //   num_of_articles: 82,
+      //   dwl_resources_count: 35,
+      //   discountCountdown: '4 days',
+      //   hasTag: true,
+      //   tag: 'HOT',
+      // });
     });
 };
