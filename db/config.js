1b2b83a06a4bdf3007c6efbbe1c38d4a06893f4a
Joshua Marx <marx.joshua@gmail.com>
2018-11-14 21:12:50 -0800

    created two configuations for postgres and raven

--- db/config.js
+++ db/config.js
@@ -1,7 +1,11 @@
-module.exports = {
+module.exports.pg = {
   host: 'localhost',
   user: 'root',
   password: 'password',
   port: 5432,
   database: 'udemy',
 };
+
+module.exports.raven = {
+  
+}
