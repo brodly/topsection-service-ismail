6f128eefb9cb99558a7da820befb2353d259bb4d
Joshua Marx <marx.joshua@gmail.com>
2018-11-20 16:25:46 -0800

    refactored with cleaner and simpler code

--- db/generation/insertData.js
+++ db/generation/insertData.js
@@ -1,21 +1,13 @@
 const path = require('path');
-const { performance } = require('perf_hooks');
 const db = require('../index');
 
 const file = 'outputfile.csv';
 const filepath = path.join(__dirname, file);
 
-let t0;
-let t1;
+module.exports.seed = table => db.raw(`COPY ${table} FROM '${filepath}' WITH (FORMAT csv)`)
+  .then(() => `Input ${file} into ${table}`)
+  .catch(err => err);
 
-const seed = table => new Promise((resolve, reject) => db(table)
-  .then(() => { t0 = performance.now(); })
-  .then(() => db.schema.raw(`COPY ${table} FROM '${filepath}' WITH (FORMAT csv)`))
-  .then(() => { t1 = performance.now(); })
-  .then(() => resolve(`Done! Seeding ${file} took ${t1 - t0} milliseconds`))
-  .catch(err => reject(err)));
-
-const clearTable = (table) => { db(table).del(); };
-
-module.exports.seed = seed;
-module.exports.clearTable = clearTable;
+module.exports.clearTable = table => db(table).del()
+  .then(res => res)
+  .catch(err => err);
