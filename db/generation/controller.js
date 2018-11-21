f7f345606a7f5b758e6678804ef9a2422ff84b32
Joshua Marx <marx.joshua@gmail.com>
2018-11-20 16:23:59 -0800

    implemented clearTable promise chain and destroy connection to pool after script is finished

--- db/generation/controller.js
+++ db/generation/controller.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-console */
 
+const fs = require('fs');
 const Promise = require('bluebird');
-const { performance } = require('perf_hooks');
 const generateData = require('./generateData');
 const { seed, clearTable } = require('./insertData');
+const db = require('../index');
 
-const amountPerGeneration = 10000;
-const maxItems = 30000;
+const amountPerGeneration = 100;
+const maxItems = 500;
 const table = 'courses';
+const file = 'outputfile.csv';
 
 let start = 0;
 let end = amountPerGeneration;
@@ -24,21 +26,21 @@ const promiseWhile = (condition, action) => {
   return resolver.promise;
 };
 
-const t0 = performance.now();
+clearTable(table)
+  .then((res) => { console.log(`Deleted ${res} rows in ${table}`); });
 
-clearTable(table);
-
-promiseWhile(() => end <= maxItems, () => generateData(start, end)
-  .then((res) => { console.log(res); })
-  .then(() => seed(table))
-  .then((res) => { console.log(res); })
+promiseWhile(() => end <= maxItems, () => {
+  return generateData(start, end)
+    .then((res) => { console.log(res); })
+    .then(() => seed(table))
+    .then((res) => { console.log(res); })
+    .then(() => {
+      start += amountPerGeneration;
+      end += amountPerGeneration;
+    })
+    .catch((err) => { throw err; });
+})
   .then(() => {
-    start += amountPerGeneration;
-    end += amountPerGeneration;
-  })
-  .catch(err => console.error('ERROR: ', err)))
-  .then(() => console.log(`Completed seeding '${table}' with ${maxItems} items`))
-  .then(() => {
-    const t1 = performance.now();
-    console.log(`Done! Total data generation and seeding took ${t1 - t0} milliseconds`);
+    db.destroy();
+    console.log(`Completed seeding '${table}' with ${maxItems} items`);
   });
