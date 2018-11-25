a0ae792874bb1989be07f22bfe67170a44fb627d
Joshua Marx <marx.joshua@gmail.com>
2018-11-25 12:33:13 -0800

    refactor to delete csv after generation/input complete and reordering of script execution
    generation script runs only after the table is fully cleared out

--- db/generation/controller.js
+++ db/generation/controller.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 
 const Promise = require('bluebird');
+const fs = require('fs');
+const path = require('path');
 const generateData = require('./generateData');
 const { seed, clearTable } = require('./insertData');
 const db = require('../index');
@@ -8,6 +10,9 @@ const db = require('../index');
 const amountPerGeneration = 1000000;
 const maxItems = 10000000;
 const table = 'courses';
+const csvName = 'outputfile.csv';
+
+const csvLocation = path.join(__dirname, csvName);
 
 let start = 0;
 let end = amountPerGeneration;
@@ -24,19 +29,23 @@ const promiseWhile = (condition, action) => {
   return resolver.promise;
 };
 
-clearTable(table)
-  .then((res) => { console.log(`Deleted ${res} rows in ${table}`); });
+const genscript = () => {
+  promiseWhile(() => end <= maxItems, () => generateData(start, end)
+    .then((res) => { console.log(res); })
+    .then(() => seed(table))
+    .then((res) => { console.log(res); })
+    .then(() => {
+      start += amountPerGeneration;
+      end += amountPerGeneration;
+    })
+    .catch((err) => { throw err; }))
+    .then(() => {
+      db.destroy();
+      fs.unlinkAsync(csvLocation);
+      console.log(`Completed seeding '${table}' with ${maxItems} items`);
+    });
+};
 
-promiseWhile(() => end <= maxItems, () => generateData(start, end)
-  .then((res) => { console.log(res); })
-  .then(() => seed(table))
-  .then((res) => { console.log(res); })
-  .then(() => {
-    start += amountPerGeneration;
-    end += amountPerGeneration;
-  })
-  .catch((err) => { throw err; }))
-  .then(() => {
-    db.destroy();
-    console.log(`Completed seeding '${table}' with ${maxItems} items`);
-  });
+clearTable(table)
+  .then((res) => { console.log(`Deleted ${res} rows in '${table}'`); })
+  .then(() => { genscript(); });
