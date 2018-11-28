90e5b9f06e1e93eb3a8270d91b5c92f564b09b59
Joshua Marx <marx.joshua@gmail.com>
2018-11-28 12:10:32 -0800

    commented out cassandra configuration

--- db/db.js
+++ db/db.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-console */
 const connection = require('./index');
 
+/* Comment this out when running cassandra */
 connection((err) => {
   if (err) console.log(err);
 });
 
-connection.queryAsync = (query, options = null) => connection.raw(query, options);
+connection.queryAsync = (query, options) => connection.raw(query, options);
 
 connection.queryAsync("SELECT 1 FROM pg_database WHERE datname = 'udemy'")
   .then((res) => {
@@ -17,26 +19,60 @@ connection.queryAsync("SELECT 1 FROM pg_database WHERE datname = 'udemy'")
     console.log('Database created');
   })
   .catch(() => {
-    connection.destroy();
     console.log("Database 'udemy' already exists");
   });
 
-// let queryAsync = function (query, options = null) {
-//   let connect = this;
-//   return new Promise((resolve, reject) => {
-//     connect.query(query, options, (err, results, fields) => {
-//       if(err) reject(err);
-//       else resolve(results, fields)
-//     });
-//   });
-// };
-
-// connection.on('connection', (error, connection) => {
-// if (error) console.log('error on.connection', error);
-// connection.getConnection((err, con) => {
-//   if (error) console.log('error getConneciton', err);
-//   else {
-// }
-// });
-
 module.exports = connection;
+
+/*
+Cassandra Database Setup Configuration
+*/
+
+/* Export code for courses schema used for Cassandra */
+// module.exports.makeTable = makeTable;
+
+/* Create table raw query */
+// const makeKeyspace = `
+//   CREATE KEYSPACE IF NOT EXISTS udemy
+//     WITH REPLICATION = {
+//       'class': 'SimpleStrategy',
+//       'replication_factor': 1
+//     }
+// `;
+
+/* Courses schema */
+// const makeTable = `
+//   CREATE TABLE udemy.courses(
+//     id int PRIMARY KEY,
+//     title varchar,
+//     subtitle varchar,
+//     teacher_names varchar,
+//     avg_rating int,
+//     rating_count int,
+//     student_count int,
+//     last_updated timestamp,
+//     price int,
+//     lang varchar,
+//     subtitle_lang varchar,
+//     course_len float,
+//     isOnDiscount boolean,
+//     current_price float,
+//     discount float,
+//     num_of_articles int,
+//     dwl_resources_count int,
+//     discountCountdown varchar,
+//     hasTag boolean,
+//     tag varchar,
+//     thumbnail_img text
+//   )
+// `;
+
+/* queryAsync specific to Cassandra note: .execute instead of .raw for Postgres */
+// connection.queryAsync = (query, options) => connection.execute(query, options);
+
+/* Cassandra Promise chain for connecting to database and creating table */
+// connection.queryAsync(makeKeyspace)
+//   .then(() => connection.queryAsync(makeTable))
+//   .then(() => connection.shutdown())
+//   .then(() => console.log('Database created'))
+//   .catch(err => console.log(err));
